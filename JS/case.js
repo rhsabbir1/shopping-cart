7598e9d646f63a0d53ba9e260193d448b6a71560
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-15 23:20:38 +0600

    case plus update

---
+++ JS/case.js
@@ -0,0 +1,37 @@
+function updateCaseNumber(isIncres) {
+    const inputFiled = document.getElementById('case-input');
+    const inputValuestring = inputFiled.value;
+    const previousinputValue = parseInt(inputValuestring)
+    let newInputValue;
+    if (isIncres) {
+        newInputValue = previousinputValue + 1;
+    }
+    else {
+        if (previousinputValue <= 0) {
+            return;
+        }
+        newInputValue = previousinputValue - 1;
+    }
+    inputFiled.value = newInputValue;
+    return newInputValue;
+}
+function priceUpdate(inputNUmber) {
+    const casePrice = document.getElementById('case-price')
+    
+    let newCasePRice = inputNUmber * 56;
+    casePrice.innerText = newCasePRice;
+ 
+}
+
+document.getElementById('case-plus-btn').addEventListener('click', function () {
+    const casePlusInputNumber = updateCaseNumber(true)
+
+    priceUpdate(casePlusInputNumber)
+
+})
+
+document.getElementById('case-minus-btn').addEventListener('click', function () {
+    const caseMinusInputNUmber = updateCaseNumber(false)
+
+    priceUpdate(caseMinusInputNUmber)
+})
